f1ee706068d34be434876eeeeb036b1a602a3d4d
jrhel <johannes.riska@helsinki.fi>
2020-11-24 20:25:44 +0200

    one more test

--- otRunView/src/test/java/logic/LogicTest.java
+++ otRunView/src/test/java/logic/LogicTest.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 import main.domain.Category;
 import main.domain.Logic;
+import main.domain.Run;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -45,5 +48,17 @@ public class LogicTest {
         logic.saveCategory("child", attributes, "parent");
         assertEquals("id: 2, name: child, parent: parent, attributes:\nsunny\n", logic.readCategory("child"));
     }
-
+    
+    @Test public void canCalculateAvgSpeed() {
+        double distanceKm = 21.0;
+        LocalTime time = new LocalTime(1, 40, 40);
+        LocalDate date = new LocalDate(2018-11-3);
+        int avgCadence = 154;
+        String gpx = "";
+        List<Category> categories = new ArrayList<>();        
+        Run run =  new Run(distanceKm, date, time, avgCadence, gpx, categories);
+        double hours = ((40.0 / 3600) + (40.0 / 60) + 1);
+        double expectedResult = distanceKm / hours;
+        assertEquals(expectedResult, run.calculateAvgSpeedKmH(distanceKm, time), 0.01);
+    }
 }
